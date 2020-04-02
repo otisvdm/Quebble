cba59d3800d966e111c0b0b6d19d58d8927d0ed6
Raymond de Bruine <Raymond.debruine@gmail.com>
2020-04-02 12:15:09 +0200

    antwoordfactory weg en override

--- src/main/java/Vagado/Puntentelling.java
+++ src/main/java/Vagado/Puntentelling.java
@@ -3,7 +3,6 @@ package Vagado;
 import java.util.ArrayList;
 
 public abstract class Puntentelling implements iPuntentelling {
-    public int telPunten(Ronde[] rondes) {
-        return 0;
-    }
+
+    public abstract int telPunten(Ronde[] rondes);
 }
