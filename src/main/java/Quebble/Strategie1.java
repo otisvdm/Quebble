2fdb09580b3a167913d6c1ea3eb048d5abf7a405
Raymond de Bruine <raymond.debruine@gmail.com>
2020-10-28 23:40:07 +0100

    buiten le scope

--- src/main/java/Quebble/Strategie1.java
+++ src/main/java/Quebble/Strategie1.java
@@ -1,6 +1,10 @@
 package Quebble;
 
 public class Strategie1 implements iTelstrategie {
+
+    /*
+     * De implementatie van een puntentelling algoritme valt buiten de scope van deze opdracht.
+     */
     public int telPunten(SpelerAntwoord antwoordLijst, Woord woord, String strategie) {
         return 1;
     }
--- src/main/java/Quebble/Strategie2.java
+++ src/main/java/Quebble/Strategie2.java
@@ -1,6 +1,10 @@
 package Quebble;
 
 public class Strategie2 implements iTelstrategie {
+
+    /*
+     * De implementatie van een puntentelling algoritme valt buiten de scope van deze opdracht.
+     */
     public int telPunten(SpelerAntwoord antwoordLijst, Woord woord, String strategie) {
         return 1;
     }
